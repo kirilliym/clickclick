1ced000e7b3e65f658ac7429aa2f8301613804eb
5TEMIR <stemir2015@yandex.ru>
2023-05-02 12:50:26 +0400

    Add reset

--- Assets/counter.py
+++ Assets/counter.py
@@ -13,20 +13,26 @@ class Counter(BoxLayout):
         self.counter.text_int = 0
         self.add_widget(self.counter)
 
-        self.pluss = BoxLayout()
-        self.add_widget(self.pluss)
+        self.calc = BoxLayout()
+        self.calc.orientation = "vertical"
+        self.add_widget(self.calc)
 
-        self.button_p_1 = Button(text="+1")
-        self.button_p_1.bind(on_press=self.plus_1)
+        self.pluss = BoxLayout()
+        self.calc.add_widget(self.pluss)
+
+        self.button_p_1 = Button(text="+1", on_press=self.plus_1)
         self.pluss.add_widget(self.button_p_1)
 
-        self.button_p_2 = Button(text="+2")
-        self.button_p_2.bind(on_press=self.plus_2)
+        self.button_p_2 = Button(text="+2", on_press=self.plus_2)
         self.pluss.add_widget(self.button_p_2)
 
-        self.button_p_3 = Button(text="+3")
-        self.button_p_3.bind(on_press=self.plus_3)
+        self.button_p_3 = Button(text="+3", on_press=self.plus_3)
         self.pluss.add_widget(self.button_p_3)
+
+        self.reset = BoxLayout()
+        self.button_del = Button(text="reset", on_press=self.to_reset)
+        self.reset.add_widget(self.button_del)
+        self.calc.add_widget(self.reset)
 
     def update_counter(self, instance):
         self.counter.text = str(self.counter.text_int)
@@ -41,4 +47,8 @@ class Counter(BoxLayout):
 
     def plus_3(self, instance):
         self.counter.text_int += 3
+        self.update_counter(instance)
+
+    def to_reset(self, instance):
+        self.counter.text_int = 0
         self.update_counter(instance)
