e9519ec7666f2e879a2218536e813ec0b985486b
5TEMIR <stemir2015@yandex.ru>
2023-05-25 21:51:22 +0400

    Edit shadow

--- Assets/custom_buttons.py
+++ Assets/custom_buttons.py
@@ -11,11 +11,11 @@ class ClickButton(Button):
 
     def on_press(self):
         self.color_shadow = (0 / 255, 0 / 255, 0 / 255, 0.55)
-        self.shadow_slip = (6, -6)
+        self.shadow_slip = (1.06, 0.94)
         self.default_size_hint = self.size_hint[0]
         self.size_hint = (self.size_hint[0] * 0.99, None)
 
     def on_touch_up(self, touch):
         self.color_shadow = (0 / 255, 0 / 255, 0 / 255, 0.5)
-        self.shadow_slip = (7, -7)
+        self.shadow_slip = (1.07, 0.93)
         self.size_hint = (self.default_size_hint, None)
