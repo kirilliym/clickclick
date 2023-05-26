e36b1aedc7e329378db2228adea8dbc90eba4819
Kirill <fikfack806@mail.ru>
2023-05-27 01:13:19 +0400

    was added database for counter

--- Assets/counter.py
+++ Assets/counter.py
@@ -2,6 +2,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from Assets.custom_buttons import TestButton
+import sqlite3 as sql
 
 
 class Counter(BoxLayout):
@@ -13,8 +14,31 @@ class Counter(BoxLayout):
         self.add_widget(self.test_button)
 
         self.counter = Label()
-        self.counter.text = "0"
-        self.counter.text_int = 0
+
+        connection = sql.connect("database.db")
+        cursor = connection.cursor()
+        query = "CREATE TABLE IF NOT EXISTS baza (count INTEGER)"
+        cursor.execute(query)
+        connection.commit()
+        
+        k = 0
+        query = "SELECT * FROM baza"
+        cursor.execute(query)
+        got = cursor.fetchone()
+        
+        if got == None:
+            query = "INSERT INTO baza (count) VALUES (?)"
+            values = (0,)
+            cursor.execute(query, values)
+            connection.commit()
+        else:
+            k = got[0]
+        
+        connection.close()
+        
+
+        self.counter.text = str(k)
+        self.counter.text_int = k
         self.add_widget(self.counter)
 
         self.calc = BoxLayout()
@@ -27,32 +51,19 @@ class Counter(BoxLayout):
         self.button_p_1 = Button(text="+1", on_press=self.plus_1)
         self.pluss.add_widget(self.button_p_1)
 
-        self.button_p_2 = Button(text="+2", on_press=self.plus_2)
-        self.pluss.add_widget(self.button_p_2)
-
-        self.button_p_3 = Button(text="+3", on_press=self.plus_3)
-        self.pluss.add_widget(self.button_p_3)
-
-        self.reset = BoxLayout()
-        self.button_del = Button(text="reset", on_press=self.to_reset)
-        self.reset.add_widget(self.button_del)
-        self.calc.add_widget(self.reset)
-
     def update_counter(self, instance):
         self.counter.text = str(self.counter.text_int)
 
     def plus_1(self, instance):
+
+        connection = sql.connect("database.db")
+        cursor = connection.cursor()
+        update = "UPDATE baza SET count = ? WHERE count = ?"
+        values = (self.counter.text_int + 1, self.counter.text_int)
+        cursor.execute(update, values)
+        connection.commit()
+        connection.close()
+
         self.counter.text_int += 1
         self.update_counter(instance)
 
-    def plus_2(self, instance):
-        self.counter.text_int += 2
-        self.update_counter(instance)
-
-    def plus_3(self, instance):
-        self.counter.text_int += 3
-        self.update_counter(instance)
-
-    def to_reset(self, instance):
-        self.counter.text_int = 0
-        self.update_counter(instance)
